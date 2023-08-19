9b09c0757530da719b7bf1053579efcdf660a475
SimgeAkbulut <simgeakbulut@icloud.com>
2023-08-19 15:35:58 -0400

    Implement the add button

--- gui.py
+++ gui.py
@@ -5,7 +5,6 @@ label = sg.Text("Type in a to-do :")
 input_box = sg.InputText(tooltip= "Enter Box", key ='todo')
 add_button = sg.Button('Add')
 
-
 window = sg.Window("My To-Do App",
                    layout = [[label], [input_box, add_button]],
                    font= ('Helvetica',15))
