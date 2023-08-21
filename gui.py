87d214b8aa14478455e70884d6214a7b159c3b62
SimgeAkbulut <simgeakbulut@icloud.com>
2023-08-21 02:02:26 -0400

    Implement all the buttons

--- gui.py
+++ gui.py
@@ -4,21 +4,55 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do :")
 input_box = sg.InputText(tooltip= "Enter Box", key ='todo')
 add_button = sg.Button('Add')
+list_box = sg.Listbox(values = functions.get_todos(), key = 'todos',
+                      enable_events=True, size=[45,10])
+edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
 window = sg.Window("My To-Do App",
-                   layout = [[label], [input_box, add_button]],
+                   layout = [[label], [input_box, add_button],
+                             [list_box,edit_button,complete_button],
+                             [exit_button]],
                    font= ('Helvetica',15))
-event, values= window.read()
-print(event)
-print(values)
 
-match event:
+match True:
+    event, values= window.read()
+    print(event)
+    print(values)
+
     case 'Add':
         todos = functions.get_todos()
+
         new_todo = values['todo'] + '\n'
         todos.append(new_todo)
+
         functions.write_todos(todos)
+        window['todos'].update(values= todos)
+    case 'Edit':
+        todo_to_edit = values['todos'][0]
+        new_todo = values['todo']
 
+        todos= functions.get_todos()
 
+        index= todos.index(todo_to_edit)
+        todos[index] = new_todo
+
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+
+    case 'Complete':
+        todo_to_complete = values['todos'][0]
+
+        todos = functions.get_todos()
+
+        todos.remove(todo_to_complete)
+
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value = '')
+    case 'todos':
+        window['todo'].update(values= values['todos'][0])
+    case 'Exit':
 
 window.close()
