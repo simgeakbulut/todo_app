55ca59f4899174ff9b678f644bc37fdd1d85d1ff
SimgeAkbulut <simgeakbulut@icloud.com>
2023-08-21 02:36:18 -0400

    Implement all the buttons

--- gui.py
+++ gui.py
@@ -2,10 +2,10 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do :")
-input_box = sg.InputText(tooltip= "Enter Box", key ='todo')
+input_box = sg.InputText(tooltip="Enter Box", key='todo')
 add_button = sg.Button('Add')
 
-list_box = sg.Listbox(values = functions.get_todos(), key = 'todos',
+list_box = sg.Listbox(values = functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button('Edit')
 complete_button = sg.Button('Complete')
@@ -13,10 +13,10 @@ exit_button = sg.Button('Exit')
 
 
 window = sg.Window("My To-Do App",
-                   layout = [[label], [input_box, add_button],
-                             [list_box,edit_button,complete_button],
+                   layout=[[label], [input_box, add_button],
+                             [list_box, edit_button, complete_button],
                              [exit_button]],
-                   font= ('Helvetica',15))
+                   font=('Helvetica', 15))
 
 while True:
     event, values = window.read()
@@ -31,12 +31,12 @@ while True:
             todos.append(new_todo)
 
             functions.write_todos(todos)
-            window['todos'].update(values= todos)
+            window['todos'].update(values=todos)
         case 'Edit':
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
 
-            todos= functions.get_todos()
+            todos=functions.get_todos()
 
             index= todos.index(todo_to_edit)
             todos[index] = new_todo
@@ -53,9 +53,9 @@ while True:
 
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-            window['todo'].update(value = '')
+            window['todo'].update(value='')
         case 'todos':
-            window['todo'].update(values= values['todos'][0])
+            window['todo'].update(values=values['todos'][0])
         case 'Exit':
             break
         case sg.WIN_CLOSED:
